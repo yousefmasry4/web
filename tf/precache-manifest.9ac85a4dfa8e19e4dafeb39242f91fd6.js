ca3f5314891d833e048e8ed5e5c5228bc45870a8
Yousseff Muhammed EL Masry <youssefmasry04@gmail.com>
2021-05-01 10:08:01 +0200

    Add files via upload

---
+++ tf/precache-manifest.9ac85a4dfa8e19e4dafeb39242f91fd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a53597ee366d933cd7c465791aaeb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b8d6e85a97cec69dbfa",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d24ee5e3079cf1083d73",
+    "url": "/static/js/2.f321324c.chunk.js"
+  },
+  {
+    "revision": "93ce851949c81c033c28f6758a3c33f3",
+    "url": "/static/js/2.f321324c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b8d6e85a97cec69dbfa",
+    "url": "/static/js/main.219a627c.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
